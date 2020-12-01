e819b707d06d6054c629dc088e3e3940df22fda0
TunkShif <tunkshif@foxmail.com>
2020-12-01 20:43:36 +0800

    feat: debug mode in main.c

--- source/main.c
+++ source/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "../include/CLI.h"
+#define DEBUG 0
 
+#if DEBUG
+int main(int argc, char *argv[]) {
+    return 0;
+}
+#else
 int main(int argc, char *argv[]) {
     if (argc == 1) {
         PromptLoop();
@@ -13,11 +19,13 @@ int main(int argc, char *argv[]) {
                 float result = CalculateFromSuffixExpression(exp);
                 printf("%s = %.2f", argv[2], result);
             }
-        } else if (strcmp(argv[1], "-f") == 0) {
-            CalcFromFile(argv[2]);
         }
+//        else if (strcmp(argv[1], "-f") == 0) {
+//            CalcFromFile(argv[2]);
+//        }
     } else {
         puts("WRONG COMMAND!");
     }
     return 0;
 }
+#endif
